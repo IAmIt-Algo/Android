486416d1cf1754bff5126466032be82f79333dd2
egorix2000 <egorix2000@mail.ru>
2016-11-06 18:44:38 +0300

    add your rating

---
+++ Game/Assets/Scripts/Menu/Rating.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Rating : MonoBehaviour {
+
+    public Text Position;
+    public Text Stars;
+    int LowestPosition = 5;
+    int HighestPosition = 2;
+    int StarsCount = 25;
+
+	// Use this for initialization
+	void Start () {
+        Position.text = HighestPosition + " - " + LowestPosition;
+        Stars.text = StarsCount + "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
