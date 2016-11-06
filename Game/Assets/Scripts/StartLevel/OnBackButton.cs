178a5edf5e2e5241344cd26fa4839f0c90511666
VladLutskevich <vlad01309@mail.ru>
2016-11-06 22:40:08 +0300

    UI fixed

---
+++ Game/Assets/Scripts/StartLevel/OnBackButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Mindblower.Gui;
+
+public class OnBackButton : MonoBehaviour {
+
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("StartLevel");
+    }
+}
